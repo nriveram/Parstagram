7997f830acd1a20d601839600d684088b7e79780
Naomi Rivera <naomirivera@Naomis-MacBook-Air.local>
2019-04-16 19:43:06 -0700

    new commit

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -92,7 +92,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let post = posts[section]
-        let comments = (post["commets"] as? [PFObject]) ?? []
+        let comments = (post["comments"] as? [PFObject]) ?? []
         return comments.count + 2
     }
     
@@ -103,7 +103,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.section]
-        let comments = (post["commets"] as? [PFObject]) ?? []
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
         if indexPath.row == 0 {
         
